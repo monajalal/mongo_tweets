06039c0883ee7817912c172e8e9be5fcf712e066
Wenjun Zhu <chaoswjz@gmail.com>
2018-10-15 11:58:19 -0400

    tweet_spider error handle

--- tweet_spider.py
+++ tweet_spider.py
@@ -6,7 +6,7 @@ import datetime as DT
 from datetime import date
 #from geopy.geocoders import Nominatim
 #from geopy.exc import GeocoderTimedOut
-#import time
+import time
 
 json_path = '../JSON_files'
 
@@ -16,6 +16,8 @@ MONGO_HOST = 'mongodb://localhost/twitterdb'
 
 #keywords
 WORDS = ['#midterm', '#2018midterms', '#election', '#november2018', '#vote2018']
+
+ERROR_LIMIT = 5
 
 '''
 def getLocation(address):
@@ -37,31 +39,50 @@ def setupTweeepy(f):
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
     return api
 
 def searchTweets(api):
     num_of_tweets = 0
+    error_counter = 0
     today = str(date.today())
     week_ago = str(date.today() - DT.timedelta(days=7))
-    for tweet in tweepy.Cursor(api.search, q='#midterm OR #2018midterms OR #election OR #november2018 OR #vote2018 '
-                                             '-filter:retweets',
-                               since=week_ago, until=today, lang='en', tweet_mode='extended').items():
-        #location = getLocation(tweet.user.location)
+    while(True):
+        try:
+            for tweet in tweepy.Cursor(api.search, q='#midterm OR #2018midterms OR #election OR #november2018 OR #vote2018 '
+                                                     '-filter:retweets',
+                                       since=week_ago, until=today, lang='en', tweet_mode='extended').items():
+                #location = getLocation(tweet.user.location)
 
-        client = MongoClient(MONGO_HOST)
-        db = client.twitterdb
-        datajson = json.dumps(tweet._json)
-        jsondata = json.loads(datajson)
-        db.twitter_search.insert(jsondata)
+                error_counter = 0
 
-        #save the json file
-        with open(json_path + '/{0}.json'.format(tweet.id), 'w+') as f:
-            json.dump(tweet._json, f, indent=4)
-        num_of_tweets += 1
-        print('get {0} tweet(s)'.format(num_of_tweets), '\n', tweet.full_text, '\n\n', 'created at',
-              tweet.created_at, '\t', tweet.user.location, '\n\n')
+                client = MongoClient(MONGO_HOST)
+                db = client.twitterdb
+                datajson = json.dumps(tweet._json)
+                jsondata = json.loads(datajson)
+                db.twitter_search.insert(jsondata)
+
+                #save the json file
+                with open(json_path + '/{0}.json'.format(tweet.id), 'a+') as f:
+                    json.dump(tweet._json, f, indent=4)
+                num_of_tweets += 1
+                #with open('../data_samples.txt', 'a+') as file:
+                #    file.write('tweet text:\n' + tweet.full_text + '\n\n')
+                print('get {0} tweet(s)'.format(num_of_tweets), '\n', tweet.full_text, '\n\n', 'created at',
+                      tweet.created_at, '\t', tweet.user.location, '\n\n')
+
+        except tweepy.TweepError as e:
+            print('Error occurs:', e.reason)
+            error_counter += 1
+            if e.api_code == 429:
+                time.sleep(15*60)
+                continue
+            if error_counter > ERROR_LIMIT:
+                break
+            else:
+                continue
+
         '''
         if location is not None:
             if 'country' in location.raw['address']:
